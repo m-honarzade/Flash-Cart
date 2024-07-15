f9c5ba330ead6e7fc5d0da7c056bd74732173f2b
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-07-15 12:52:24 +0330

    edit color

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,8 +1,8 @@
 const Cart = ({ item, active }) => {
   return (
     <div
-      className={` h-52 border border-gray-400 bg-slate-50 rounded-md p-4 flex justify-center text-center items-center ${
-        active ? "bg-red-300" : ""
+      className={` h-52 border border-gray-400  rounded-md p-4 flex justify-center text-center items-center ${
+        active ? "bg-red-300" : "bg-slate-50"
       }`}
     >
       <p>{active ? item.answer : item.question}</p>
